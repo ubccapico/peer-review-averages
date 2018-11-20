c3ddd0c435b382c630730e781d46c0264dd6251c
Hidjaja <jeremyh2@ead.ubc.ca>
2018-11-20 09:05:23 -0800

    Possible fix for uneeded data from past assignments

--- peer_review_average.py
+++ peer_review_average.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Grading
+Peer Review average script.
+
+Author(s): Victor S., Jeremy H.
 
 """
 
 #these are the packages you need to install, this will try to install them, otherwise use pip to install
-import time, getpass, requests, pandas as pd, json
+import getpass, requests, pandas as pd, json
 
 class Helper:
+    #Helper function deals with pagination
     def paginate_list(sub_list, token):
         json_list = pd.read_json(sub_list.text)
         try:
@@ -27,10 +30,6 @@ class Helper:
 
         return json_list
 
-now = time.strftime("%c")
-## date and time representation
-filename = time.strftime("%c") + ".txt"
-
 # ENTER THE FOLLOWING INFORMATION
 token = getpass.getpass("Enter your token: ")
 
@@ -44,6 +43,7 @@ course = input("Enter Canvas course ID: ")
 assignment_id = input("Enter Canvas assignment ID: ")
 
 #get the assignment information
+print("Please wait (1/4). Gathering assignment information...")
 assignmentInfo = requests.get(url + '/api/v1/courses/' + str(course) + '/assignments/' + assignment_id,
                  headers= {'Authorization': 'Bearer ' + token})
 
@@ -55,6 +55,7 @@ rubric_id = str(assignmentInfo['rubric_settings']['id'])
 # the artifact_id = the assignment id of the assignmentSubmissions
 # the assessor_id = the person who assessed the assignment
 # the score = the total score (sum of rubric points)
+print("Please wait (2/4). Gathering peer reviews...")
 payload = {'include': 'peer_assessments',
            'style' : 'full'}
 r = requests.get(url + '/api/v1/courses/' + str(course) + '/rubrics/' + rubric_id,
@@ -80,13 +81,16 @@ peerReview_df = pd.read_json(peerReview.text)
 
 peerReview_df['user_id'] = peerReview_df['user_id'].astype(str)
 
+#Merge two dataframes to provide combined data on peer review
 merged_df = pd.merge(peerReview_df, assessments_df, how='inner', left_on=['assessor_id', 'asset_id'], right_on=['assessor_id', 'artifact_id'])
 
 #Get student list
+print("Please wait (3/4). Gathering student information...")
 student_list = requests.get('{}/api/v1/courses/{}/users'.format(url, course),
                             params = {'enrollment_type[]': 'student'},
                             headers = {'Authorization': 'Bearer ' + token})
 
+#Decides whether SIS student ID data is missing
 no_student_id = False
 if student_list.ok:
     student_df = Helper.paginate_list(student_list, token)
@@ -95,12 +99,12 @@ if student_list.ok:
     except:
         student_df = student_df[['id', 'name']]
         no_student_id = True
-    
-    student_df.to_csv('testing.csv')
 else:
     print(student_list)
-    print("Not allowed.")
+    input("Aborted, cannot get student data, {}. Press any key to exit: ".format(student_list))
+    exit()
 
+#Merge 
 merged_df = pd.merge(merged_df, student_df, how='inner', left_on=['assessor_id'], right_on=['id'])
 if(no_student_id):
     merged_df = merged_df.rename(columns={"name":"assessor_name"}).drop(['id'], axis=1)
@@ -126,17 +130,23 @@ merged_df.to_csv('{}_peer review information.csv'.format(course), index=False)
 
 #create the meanscore table with user_id and mean score
 # make sure the mean score is rounded to 2, and the user_id is a string
+print("Please wait (4/4). Calculating averages...")
 meanScore = pd.DataFrame(merged_df.groupby('user_id')['score'].mean().round(2).reset_index())
 meanScore = meanScore[pd.notnull(meanScore['score'])]
 meanScore['user_id'] = meanScore['user_id'].astype(str)
-meanScore.to_csv("course_" + course + "_" + "assignment_" + assignment_id + '_complete mean score.csv')
-#f.write("course_" + course + "_" + "assignment_" + assignment_id + '_complete mean score.csv' + "file created \n")
 
+#Associating student information with average scores
+meanScore = meanScore.merge(student_df, meanScore, how='left', left_on['id'], right_on['user_id'])
+meanScore = meanScore.drop(['user_id'], axis=1)
+
+#Write to CSV
+meanScore.to_csv("course_" + course + "_" + "assignment_" + assignment_id + '_complete mean score.csv')
+
+'''
 #whether to upload the data to the gradebook = True OR not = False
 upload = False
 #upload mean score to GradeCentre
 if upload==True:
-    #f.write("data should be uploaded to gc \n")
     for index, row in meanScore.iterrows():
 
         student_id = str(row['user_id'])
@@ -153,6 +163,4 @@ if upload==True:
         r = requests.put(url + '/api/v1/courses/' + str(course) + '/assignments/' + str(assignment_id) +'/submissions/' + str(student_id) + '/', params=payload, headers= {'Authorization': 'Bearer ' + token})
 else:
     print('data not uploaded')
-    #f.write("data not uploaded")
-
-#f.close()
+'''
